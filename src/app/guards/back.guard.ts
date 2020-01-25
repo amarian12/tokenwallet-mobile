ab7f7f4173a4326e642b17409499cf2fd3427243
TeNsHi SaMa <angel@halconmedia.com>
2020-01-25 10:52:40 -0500

    modifications on setup screen partially to align to brand, solving uppercase on email, adding guard to avoid back(partial)

---
+++ src/app/guards/back.guard.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { LocalStorageService } from 'ngx-store';
+import { LogService } from '../providers/log.service';
+import { AppConstants } from '../domains/app-constants';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BackGuard implements  CanActivate{
+  constructor(
+    private router: Router,
+    private logger: LogService,
+    private localStorageService: LocalStorageService
+  ){
+
+  }
+ canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+      const walletAlreadySetup = this.localStorageService.get(AppConstants.KEY_SETUP_COMPLETED);
+      this.logger.debug("#### BackGuard Wallet Setup is finished?: "+walletAlreadySetup);
+      if (walletAlreadySetup){
+        this.router.navigateByUrl('/',{replaceUrl:true})
+      }
+      return !walletAlreadySetup;
+
+  }
+ // canLoad(
+ //    route: ActivatedRouteSnapshot): Observable<boolean> | Promise<boolean> | boolean  {
+ //
+ //      const walletAlreadySetup = this.localStorageService.get(AppConstants.KEY_SETUP_COMPLETED);
+ //      this.logger.debug("#### BackGuard Wallet Setup is finished?: "+walletAlreadySetup);
+ //      if (walletAlreadySetup){
+ //        this.router.navigateByUrl('/',{replaceUrl:true})
+ //      }
+ //      return !walletAlreadySetup;
+ //
+ //  }
+
+}
